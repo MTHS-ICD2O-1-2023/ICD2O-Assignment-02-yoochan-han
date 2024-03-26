33ca235145761ea377ab059a7d7504bf24eb89cc
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-26 18:38:47 +0000

    Final

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ function Caculate() {
   const height = parseInt(document.getElementById("get_height").value)
   // caculate
   const answer = (base * height) * (1/3)
-  //display
+  //display 
   document.getElementById("Answer").innerHTML = "volume of pyramid that has " + base + "cm² base and " + height + "cm height is " + answer +  "cm³."
 }
