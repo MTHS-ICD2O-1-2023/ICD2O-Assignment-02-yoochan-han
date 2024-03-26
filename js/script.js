dcd984ded5c4a78d72c6ac71b680f99cb25e2623
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-26 18:41:11 +0000

    #

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Yoochan 
+// Created by: Yoochan
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
 function Caculate() {
