ccb6e3c735958f12831469bdf1221ee8c660c389
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-26 17:51:42 +0000

    #

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yoochan 
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+function CaculateToClick() {
+  const base = parseInt(document.getElementById("get_base").value)
+  const height = parseInt(document.getElementById("get_height").value)
+  // caculate
+  const result = 1/3(base*height)
+  document.getElementById("ResultOfPyramid").innerHTML = "volume of pyramid that has " + base + "cm² base and " + height + "cm height is " + result +  "cm³."
+}
