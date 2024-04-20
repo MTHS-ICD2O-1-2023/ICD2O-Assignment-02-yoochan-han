a01e4bddf1768cf791b5e177a557982587178857
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-20 04:55:27 +0000

    #

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 // Created by: Yoochan
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
-function Caculate() {
+function Calculate() {
   const base = parseInt(document.getElementById("get_base").value)
   const height = parseInt(document.getElementById("get_height").value)
-  // caculate
+  // calculate
   const answer = (base * height) * (1/3)
   //display 
   document.getElementById("Answer").innerHTML = "volume of pyramid that has " + base + " cm² base and " + height + " cm height is " + answer +  "cm³."
